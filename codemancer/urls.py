f82dc33f8abe0f016b54e4b42722602f10b7406b
chris <chrisrhayden@gmail.com>
2017-08-31 14:51:36 -0700

    cleaned git tracing

--- codemancer/urls.py
+++ codemancer/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from pages.views import view_code
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^display/', view_code, name='code_display'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_code(request):
+    """ render the code to page """
+
+    return render(request, 'code_display.html')
