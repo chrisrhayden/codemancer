560ae59f0c9dad483047eb1f85e2b956d009a3b1
chris <chrisrhayden@gmail.com>
2017-09-14 15:28:58 -0700

    worked on layout

--- pages/static/pages/js/detail.js
+++ pages/static/pages/js/detail.js
@@ -2,22 +2,14 @@ $(document).ready(function() {
     // for detail only
     'use strict';
     let two_click = 0;
-    $('pre').on('click', function() {
-        let $chiliins = $(this).children();
-        let $code_span = $($chiliins[0]);
-        let $code_code = $($chiliins[1]);
-        let $line_num = $code_code.attr('data-line');
-
+    $('.line-number').on('click', function() {
+        let line_num = $(this).text();
         if (two_click === 0) {
-            $('#id_line_begin').val(Number($line_num));
+            $('#id_line_begin').val(Number(line_num));
             two_click = 1;
         } else {
-            $('#id_line_end').val(Number($line_num));
+            $('#id_line_end').val(Number(line_num));
         }
-
-        $code_span.css({
-            'color': 'green'
-        });
     });
     $('.ano-submit').on('click', function(evt) {
         evt.preventDefault();
